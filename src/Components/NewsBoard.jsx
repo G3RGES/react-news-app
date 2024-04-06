9a54ce1e7f73ceeefdf71746db1acea99edfaf18
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-06 12:34:21 +0200

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,12 +1,33 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import NewsItem from "./NewsItem";
 
 const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=eg&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
+
   return (
     <div>
       <h2 className="text-center">
-        Latest <span className="">News</span>
+        Latest<span className="badge bg-danger">News</span>
       </h2>
+      {articles.map((news, idx) => (
+        <NewsItem
+          key={idx}
+          title={news.title}
+          description={news.description}
+          src={news.urlToImage}
+          url={news.url}
+        />
+      ))}
     </div>
   );
 };
