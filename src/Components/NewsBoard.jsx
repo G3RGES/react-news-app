2f36e5fce4cebed75e5b9f99ebc9ac2e5f68722d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-06 12:03:58 +0200

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const NewsBoard = () => {
-  return <div>NewsBoard</div>;
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="">News</span>
+      </h2>
+    </div>
+  );
 };
 
 export default NewsBoard;
